b51b2fbeb9d2c6664ca5a9ee4116376c7b6ed741
Muskan Gyanani <111181668+muskaann129@users.noreply.github.com>
2023-06-06 11:34:11 +0000

    challenge 5 completed

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,13 @@
-// Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username] : "muskaann.129",
+  [password] : "highlysecuredpassmuskan",
+  age : 17
+}
+
+// by applying the square brackets the symbol item becomes private and do not show the value of the item 
+
+console.log(user.username);
+console.log(user.password);
