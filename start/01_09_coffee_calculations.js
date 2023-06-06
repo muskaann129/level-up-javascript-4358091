2d134a02684a95bf685baf123d6eb5e8e544c077
Muskan Gyanani <111181668+muskaann129@users.noreply.github.com>
2023-06-06 14:17:42 +0000

    challenge 9 completed

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,8 @@
 // Write your code here
+function totalBill(coffeeArr){
+  let coffeeSum = coffeeArr.reduce(
+    (totalCoffees, numCoffees) => (totalCoffees =+ numCoffees)
+  );
+  return `The total price is $${coffeeSum * 1.25}`;
+}
+console.log(totalBill([2,5,7,1,4]));
