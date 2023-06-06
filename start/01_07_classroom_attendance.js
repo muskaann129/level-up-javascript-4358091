ffe10243d765ac7309c1c88a5c9d5799a721006d
Muskan Gyanani <111181668+muskaann129@users.noreply.github.com>
2023-06-06 13:34:24 +0000

    challenge 7 completed

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,26 @@
 // Write your code here
+
+function getStudents(classroom){
+  let {hasTeachingAssistant, classList} = classroom;
+  let teacher, teachingAssistant, students;
+
+  if(hasTeachingAssistant){
+    [teacher, teachingAssistant, ...students] = classList
+  }
+  else{
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+// no teaching assistant
+console.log(getStudents({
+  hasTeachingAssistant: false,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Lukas"]
+}));
+
+// has teaching assistant
+console.log(getStudents({
+  hasTeachingAssistant: true,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Lukas", "Mona", "Ruby"]
+}));
