706058a29116a5076fb3967c764fc39622ea3262
Muskan Gyanani <111181668+muskaann129@users.noreply.github.com>
2023-06-06 16:56:42 +0000

    challange 12 completed

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,25 @@
 // Write your code here
+// function numPermutation(letter){
+//   let total;
+//   for(let i=1; i<=letter.length; i++){
+//     if(total === undefined){
+//       total = 1;
+//       continue;
+//     }
+//     total *= i;
+//   }
+//   return total;
+// }
+
+// by recursion
+
+function numPermutation(letter){
+  if(letter.length === 1){
+    return 1;
+  }
+  else{
+    return letter.length * numPermutation(letter.slice(1));
+  }
+}
+
+console.log(numPermutation("Hello"));
