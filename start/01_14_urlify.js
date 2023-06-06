91d76bdf50df57e3fb515e7ac78fde3eab7dbce6
Muskan Gyanani <111181668+muskaann129@users.noreply.github.com>
2023-06-06 17:23:55 +0000

    challege 14 completed

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,10 @@
 // Write your code here
+function urlify(blogTitle){
+  const punctuations = /[.,/#!$%^&*;:{}=!\-_`~()'"]/g;
+  const blogTitleWithoutPunctuations = blogTitle.replace(punctuations, "");
+  
+  return blogTitleWithoutPunctuations.toLowerCase().trim().replaceAll(" ", "-");
+}
+
+console.log(urlify("Hey How are you doing!!!!"));
+console.log(urlify("I Love programming."));
