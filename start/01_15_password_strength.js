64b2d6a26f442b8a2fee593e9e5a1166362481b4
Muskan Gyanani <111181668+muskaann129@users.noreply.github.com>
2023-06-09 13:04:19 +0000

    All challanges completed

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,19 @@
 // Write your code here
+function checkPassword(password){
+  let oneLowerCaseLetter = /(?=.+[a-z])/;
+  let oneUpperCaseLetter = /(?=.+[A-Z])/;
+  let oneDigit = /(?=.+[0-9])/;
+  let oneSpecialCharacter = /(?=.+[!@#$%^&*])/;
+  let miminumEightCharacter = /(?=.{8,})/;
+  let isValid = 
+  oneLowerCaseLetter.test(password) &&
+  oneUpperCaseLetter.test(password) &&
+  oneDigit.test(password) &&
+  oneSpecialCharacter.test(password) &&
+  miminumEightCharacter.test(password);
+
+  return isValid ? "Your password is valid" : "Invalid Password";
+}
+
+console.log(checkPassword("JDjd56#hdc"));
+console.log(checkPassword("Muskan"));
